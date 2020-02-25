0d799e61047f0e529381d507e5a65e78ae2b771f
mousse <hpmousse@gmail.com>
2020-02-25 21:57:47 +0100

    commit122

--- app/config.inc.php
+++ app/config.inc.php
@@ -2,6 +2,6 @@
 
 // Configure this file
 
-define('PUBLIC_KEY', '');
-define('SECRET_KEY', '');
+define('PUBLIC_KEY', 'S3HRKiyOpoKAyCHNB8RCZqRoV1w');
+define('SECRET_KEY', 'qKVuYvpguf2nC_HdHsGK4');
 
