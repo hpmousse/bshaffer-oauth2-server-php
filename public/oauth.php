55c077c3441867f2feed78954fb010fc9dc94b2c
mousse <hpmousse@gmail.com>
2020-03-01 22:29:33 +0100

    commit

--- public/oauth.php
+++ public/oauth.php
@@ -12,6 +12,16 @@ use OAuth_io\OAuth;
 $oauth = new OAuth();
 $oauth->initialize(PUBLIC_KEY, SECRET_KEY);
 
+  $request_object = $oauth->auth('spotify', array(
+    'credentials' => $credentials
+  ));
+
+  print "<br>printr:";print_r($request_object);
+  print "<br>vardump:";var_dump($request_object);
+
+
+
+
 include 'tmpl/header.php';
 
 ?>
@@ -24,62 +34,8 @@ include 'tmpl/header.php';
 <div class="row">
   <div class="col-md-6 col-md-offset-1">
 	<div class="panel panel-default">
-      <div class="panel-heading">LOG IN</div>
-      <div class="panel-body">
-        <form class="form-horizontal">
-          <div class="form-group">
-            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-10">
-              <input type="email" class="form-control" placeholder="Email">
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" placeholder="Password">
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              <div class="checkbox">
-                <label><input type="checkbox"> Remember me</label>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              <button type="submit" class="btn btn-default">Sign in</button>
-            </div>
-          </div>
-        </form>
-        <hr>
-		<div class="container">
-		  <div class="row">
-			<div class="col-md-1">
-			</div>
-            <div class="col-md-2">
-              <a href ="../login.php?p=google">
-                <i class="fa fa-google fa-3x"></i>
-                <b>Google+</b>
-              </a>
-            </div>
-            <div class="col-md-2">
-              <a href ="../login.php?p=facebook">
-                <i class="fa fa-facebook fa-3x"></i>
-                <b>Facebook</b>
-              </a>
-            </div>
-            <div class="col-md-2">
-              <a href ="../login.php?p=spotify">
-                <i class="fa fa-spotify fa-3x"></i>
-                <b>Spotify</b>
-              </a>
-            </div>
-            <div class="col-md-1">
-            </div>
-          </div>
-        </div>
-      </div>
+
+
 	</div>
   </div>
 </div>
